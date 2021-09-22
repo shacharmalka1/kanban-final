2660d98b4f5471cc99aabd675fe0586210098abe
shacharmalka1 <shacharmalka0204@gmail.com>
2021-09-22 16:57:09 +0300

    html template

---
+++ solution/index.js
@@ -0,0 +1,17 @@
+// const sectionToDo = document.getElementById('to-do-tasks')
+// const ulTodo = document.createElement('ul')
+// ulTodo.classList.add('to-do-tasks')
+// sectionToDo.append(ulTodo)
+// const liInputToDo = document.createElement('li')
+// liInputToDo.classList.add('task')
+// const inputToDo = document.createElement('input')
+// const liButtontToDo = document.createElement('li')
+// liButtontToDo.classList.add('task')
+// const buttonToDo = document.createElement('button')
+// inputToDo.id = 'add-to-do-task'
+// buttonToDo.id = 'submit-add-to-do'
+// liInputToDo.append(inputToDo)
+// liButtontToDo.append(buttonToDo)
+// ulTodo.append(liInputToDo)
+// ulTodo.append(liButtontToDo)
+/////////////////////////////////
